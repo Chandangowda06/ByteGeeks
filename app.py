2fa2ca295250104a5a4980b89c91b846bedc6230
chandangowda06 <chandangowda06102003@gmail.com>
2023-07-16 19:23:29 +0530

    added some styles

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    cur_year = datetime.now().year
+    return render_template('home.html',year=cur_year )
 
 if __name__ == '__main__':
     app.run(debug=True)
